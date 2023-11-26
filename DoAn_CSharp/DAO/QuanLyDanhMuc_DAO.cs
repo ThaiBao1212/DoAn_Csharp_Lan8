3f475330e5e7ccddfba4cfda9657ffb22e469121
ThaiBao1212 <96051588+ThaiBao1212@users.noreply.github.com>
2023-11-26 16:35:06 +0700

    aa

---
+++ DoAn_CSharp/DAO/QuanLyDanhMuc_DAO.cs
@@ -0,0 +1,26 @@
+﻿using DoAn_CSharp.Databsase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_CSharp.DAO
+{
+    internal class QuanLyDanhMuc_DAO
+    {
+        private Database db;
+        public QuanLyDanhMuc_DAO()
+        {
+            db = new Database();
+
+        }
+        public DataTable LayDanhSachDanhMuc()
+        {
+            string strSQL = "Select * from danhmuc";
+            DataTable dt = db.Execute(strSQL);
+            return dt;
+        }
+    }
+}
